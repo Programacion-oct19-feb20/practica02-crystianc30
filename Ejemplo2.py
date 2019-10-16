968c2af80e10a9406a930dc66987730526f0c8c7
crystianc30 <crystian_carrion8@hotmail.com>
2019-10-16 00:27:23 -0500

    ejemplo2

---
+++ Ejemplo2.py
@@ -0,0 +1,9 @@
+'''
+@Crystian Carrion
+ejemplo
+'''
+print("Crystian Carrion")
+print("Loja")
+print("Loja")
+print("La Argelia")
+print("cacarrion15@utpl.edu.ec")
---
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author Salas
+ */
+public class Ejemplo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Crystian Carrion");
+                System.out.print("Loja");
+                System.out.print("Loja");
+                System.out.print("La Argelia");
+                System.out.print("cacarrion15@utpl.edu.ec");
+                
+    }
+    
+}
